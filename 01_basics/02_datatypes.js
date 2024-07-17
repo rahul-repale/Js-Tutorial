d0f625e00188a6ff4d88ba8e199ef1dcd7504276
Rahul Repale <workwithrahul14@gmail.com>
2024-07-17 20:55:15 +0530

    datatypes notes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //uses newer version of JS
+
+// alert(3+3) we are using nodejs not a browser
+
+let Name = "Rahul" //string
+let age = 20 //number => 2 to power of 53
+// bigint => use to take number bigger than 2 to power of 53 //in Trading
+let study; //undefined
+let temperature = null //null => standalone
+let IsLoggedIn = true // boolean => true/false
+// symbol => unique
+// object
+
+console.log(typeof undefined);//output => undefined
+console.log(typeof null); //output => object
+
+console.table([Name, age, study, temperature, IsLoggedIn]);
